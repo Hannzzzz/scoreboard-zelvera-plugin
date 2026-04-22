fead3771907b33b04839ae62fcf51a9b54a870a0
Reyhan Frederick <reyhanfrederick@example.com>
2026-04-22 18:05:44 +0700

    Added some changes to unused line of codes and imports

--- src/main/java/com/zelvera/scoreboard/BridgeFightIntegration.java
+++ src/main/java/com/zelvera/scoreboard/BridgeFightIntegration.java
@@ -1,7 +1,7 @@
 package com.zelvera.scoreboard;
 
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
+//import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.scoreboard.*;
--- src/main/java/com/zelvera/scoreboard/LeaderboardCommand.java
+++ src/main/java/com/zelvera/scoreboard/LeaderboardCommand.java
@@ -3,7 +3,7 @@ package com.zelvera.scoreboard;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
+//import org.bukkit.entity.Player;
 
 import java.util.List;
 import java.util.Map;
